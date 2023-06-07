08aba2af23dff90569cb49b79e5feb0314fe9347
Aleksander Kleinszmidt <60325364+Xarwex@users.noreply.github.com>
2023-06-07 08:54:22 +0100

    clean up

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ const GRID_HEIGHT: usize = 1;
 
 fn main() {
     App::new()
-        .init_resource::<Game>()
         .add_plugins(DefaultPlugins)
         .add_plugins(DefaultPickingPlugins)
         .add_startup_system(setup)
@@ -18,43 +17,34 @@ fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
-    mut game: ResMut<Game>,
 ) {
     commands.spawn((Camera2dBundle::default(), RaycastPickCamera::default()));
 
     let color = materials.add(ColorMaterial::from(Color::GREEN));
     let quad = meshes.add(Mesh::from(shape::Quad::default()));
 
-    // let mut grid = vec![];
-    for height in 0..GRID_HEIGHT {
-        // let mut line = vec![];
-        for width in 0..GRID_WIDTH {
+    for y in 0..GRID_HEIGHT {
+        for x in 0..GRID_WIDTH {
             commands.spawn((
                 MaterialMesh2dBundle {
                     mesh: quad.clone().into(),
                     transform: Transform::default()
                         .with_scale(Vec3::splat(64.))
                         .with_translation(Vec3 {
-                            x: (width * 65) as f32,
-                            y: (height * 65) as f32,
+                            x: (x * 65) as f32,
+                            y: (y * 65) as f32,
                             z: 0.,
                         }),
                     material: color.clone(),
                     ..Default::default()
                 },
-                Block {},
+                Block { x, y },
                 RaycastPickTarget::default(),
                 Pickable,
                 Interaction::default(),
             ));
-            // line.push(Block {
-            //     mesh:
-            // })
         }
-        // grid.push(line);
     }
-
-    // game.grid = grid;
 }
 
 fn click_on_block_system(
@@ -68,10 +58,8 @@ fn click_on_block_system(
     }
 }
 
-#[derive(Resource, Default)]
-struct Game {
-    grid: Vec<Vec<Block>>,
-}
-
 #[derive(Component)]
-struct Block {}
+struct Block {
+    x: usize,
+    y: usize,
+}
