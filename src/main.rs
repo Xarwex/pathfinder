04b10a4d0f1b9c6f829e846e5c6a55d8e3cde5ed
Aleksander Kleinszmidt <60325364+Xarwex@users.noreply.github.com>
2023-06-08 23:53:20 +0100

    rotatas

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle, utils::HashSet};
 use bevy_mod_picking::prelude::*;
 
-const GRID_WIDTH: usize = 5;
-const GRID_HEIGHT: usize = 1;
+const GRID_WIDTH: isize = 5;
+const GRID_HEIGHT: isize = 3;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(DefaultPickingPlugins)
+        .add_event::<RotateEvent>()
         .add_startup_system(setup)
         .add_system(click_on_block_system)
+        .add_system(resolve_rotation_system)
         .run();
 }
 
@@ -48,18 +50,54 @@ fn setup(
 }
 
 fn click_on_block_system(
+    mut ev_rotate: EventWriter<RotateEvent>,
     mut clicked: Query<(&Interaction, &mut Transform, &mut Block), Changed<Interaction>>,
 ) {
     for (interaction, mut transform, block) in &mut clicked {
         if matches!(interaction, Interaction::Clicked) {
-            // rotate the block and change the flow
-            transform.rotate_local_z(1.0);
+            ev_rotate.send(RotateEvent(block.clone()))
         }
     }
 }
 
-#[derive(Component)]
-struct Block {
-    x: usize,
-    y: usize,
+fn resolve_rotation_system(
+    mut ev_rotate: EventReader<RotateEvent>,
+    mut blocks: Query<(&mut Transform, &Block)>,
+) {
+    let blocks_affected: HashSet<Block> = ev_rotate
+        .iter()
+        .map(|rotate_event| rotate_event.0.get_adjacent())
+        .flatten()
+        .collect();
+    for (mut transform, block) in &mut blocks {
+        if blocks_affected.contains(block) {
+            transform.rotate_local_z(1.0)
+        }
+    }
 }
+
+#[derive(Component, Clone, Copy, PartialEq, Eq, Hash)]
+struct Block {
+    x: isize,
+    y: isize,
+}
+
+impl Block {
+    fn is_adjacent(self, block: &Block) -> bool {
+        [(self.x, block.x), (self.y, block.y)]
+            .iter()
+            .fold(false, |acc, (a, b)| acc ^ ((a - b).abs() == 1))
+    }
+
+    fn get_adjacent(self) -> HashSet<Block> {
+        [(1, 0), (-1, 0), (0, 1), (0, -1)]
+            .iter()
+            .map(|(x, y)| Block {
+                x: self.x + x,
+                y: self.y + y,
+            })
+            .collect()
+    }
+}
+
+struct RotateEvent(pub Block);
